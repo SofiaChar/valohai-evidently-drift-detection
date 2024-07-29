f4c59284dcd7388dde823f0559187091f70afde2
sofiacharnota <sofiacharnota@gmail.com>
2024-07-29 14:44:11 +0200

    Edit prep step to enable trigger

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,17 @@
 import json
-
+import os
 import pandas as pd
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 
-# Load the dataset
-data = fetch_california_housing(as_frame=True)
-housing_data = data.frame
+
+if os.listdir('/valohai/inputs/dataset'):
+    the_file = os.listdir('/valohai/inputs/dataset')[0]
+    housing_data = pd.read_csv(os.path.join("/valohai/inputs/dataset/", the_file))
+else:
+    # Load the dataset
+    data = fetch_california_housing(as_frame=True)
+    housing_data = data.frame
 
 # Rename the target column
 housing_data.rename(columns={'MedHouseVal': 'target'}, inplace=True)
